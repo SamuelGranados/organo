57167ec38bad1c5d01582d15037c3894e20f52cb
Samuel Eliab Granados Figuera <samuelgranados2801@gmail.com>
2024-04-04 04:20:32 -0300

    criando um organograma

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner() {
+    // JSX
+    return (
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="banner principal da pagina do organo" />
+        </header>
+        
+    )
+}
+
+export default Banner
